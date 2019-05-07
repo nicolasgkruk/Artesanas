702ca6282178233599d6c04f9e7ce0130d285f54
Nicolás Kruk <nicolasgkruk@gmail.com>
2019-05-07 20:38:52 +0200

    7 de Mayo, 20:38.

---
+++ Artesanas/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Artesanas.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Artesanas.Areas.Identity.IdentityHostingStartup))]
+namespace Artesanas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
