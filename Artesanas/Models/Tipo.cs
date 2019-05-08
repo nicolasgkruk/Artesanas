a7958d50af190e178e8977f6b83459eaaf9b8b1d
Nicolás Kruk <nicolasgkruk@gmail.com>
2019-05-08 14:27:17 +0200

    8 de Mayo, 14:27

---
+++ Artesanas/Models/Tipo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artesanas.Models
+{
+    public class Tipo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
