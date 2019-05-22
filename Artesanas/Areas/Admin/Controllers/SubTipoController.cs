03fc5a241dfc15693939619b187b1be08c6de1a4
Nicolás Kruk <nicolasgkruk@gmail.com>
2019-05-22 11:44:22 +0200

    Extension methods, TipoAndSubTipoViewModel

--- Artesanas/Areas/Admin/Controllers/SubTipoController.cs
+++ Artesanas/Areas/Admin/Controllers/SubTipoController.cs
@@ -2,16 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Artesanas.Data;
+using Artesanas.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Artesanas.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class SubTipoController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public SubTipoController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        //Get INDEX
+        public async Task<IActionResult> Index()
+        {
+            var subTipos = await _db.SubTipo.Include(s => s.Tipo).ToListAsync();
+            return View(subTipos);
+        }
+
+
+        //GET - CREATE
+        public async Task<IActionResult> Create()
+        {
+            TipoAndSubTipoViewModel model = new TipoAndSubTipoViewModel()
+            {
+                TipoList = await _db.Tipo.ToListAsync(),
+                SubTipo = new Models.SubTipo(),
+                SubTipoList = await _db.SubTipo.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ Artesanas/Models/ViewModels/TipoAndSubTipoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artesanas.Models.ViewModels
+{
+    public class TipoAndSubTipoViewModel
+    {
+        public IEnumerable<Tipo> TipoList { get; set; }
+        public SubTipo SubTipo { get; set; }
+        public List<string> SubTipoList { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
