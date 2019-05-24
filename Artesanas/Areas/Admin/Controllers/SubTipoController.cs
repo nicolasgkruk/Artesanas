ed50d6e6fa5ea8dd405cf04a1e800dc8140af5b1
Nicolás Kruk <nicolasgkruk@gmail.com>
2019-05-24 12:54:50 +0200

    Create SubTipo

--- Artesanas/Areas/Admin/Controllers/SubTipoController.cs
+++ Artesanas/Areas/Admin/Controllers/SubTipoController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Artesanas.Data;
+using Artesanas.Models;
 using Artesanas.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Artesanas.Areas.Admin.Controllers
@@ -13,6 +15,9 @@ namespace Artesanas.Areas.Admin.Controllers
     public class SubTipoController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [TempData]
+        public string StatusMessage { get; set; }
 
         public SubTipoController(ApplicationDbContext db)
         {
@@ -39,5 +44,49 @@ namespace Artesanas.Areas.Admin.Controllers
 
             return View(model);
         }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TipoAndSubTipoViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubTipoExists = _db.SubTipo.Include(s => s.Tipo)
+                    .Where(s => s.Name == model.SubTipo.Name && s.Tipo.Id == model.SubTipo.TipoId);
+
+                if (doesSubTipoExists.Count() > 0)
+                {
+                    //Error
+                    StatusMessage = "Error : SubTipo existe en la categoría:" + doesSubTipoExists.First().Tipo.Name + ". Por favor utilice otro nombre.";
+                }
+                else
+                {
+                    _db.SubTipo.Add(model.SubTipo);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            TipoAndSubTipoViewModel modelVM = new TipoAndSubTipoViewModel()
+            {
+                TipoList = await _db.Tipo.ToListAsync(),
+                SubTipo = model.SubTipo,
+                SubTipoList = await _db.SubTipo.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync(),
+                StatusMessage = StatusMessage
+            };
+            return View(modelVM);
+        }
+
+        [ActionName("GetSubTipo")]
+        public async Task<IActionResult> GetSubTipo(int id)
+        {
+            List<SubTipo> subTipos = new List<SubTipo>();
+
+            subTipos = await (from subTipo in _db.SubTipo
+                                   where subTipo.TipoId == id
+                                   select subTipo).ToListAsync();
+            return Json(new SelectList(subTipos, "Id", "Name"));
+        }
+
     }
 }
